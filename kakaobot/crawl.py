4ab0c9a5b5f6108d8edc046f48d5a555449cf770
winty95 <winty95@nate.com>
2018-08-06 10:14:56 +0900

    db date bug fix

--- kakaobot/crawl.py
+++ kakaobot/crawl.py
@@ -3,12 +3,13 @@ from kakaobot.models import Menu
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
-today=datetime.date.today().strftime("%Y년 %m월 %d일".encode('unicode-escape').decode()).encode().decode('unicode-escape')
 def crawl(request):
     '''
     일단은 DB삭제하는걸로 구현해놓고, 완성한 후에 년월일로 필터링하여 출력하는 걸로... 그럼 식단 정보도 계속 저장됨.
     datetime.date.today().strftime("%Y년 %m월 %d일".encode('unicode-escape').decode()).encode().decode('unicode-escape')
     '''
+    today = datetime.date.today().strftime("%Y년 %m월 %d일".encode('unicode-escape').decode()).encode().decode(
+        'unicode-escape')
     menu_db = Menu.objects.filter(date=today)
     # 최신상태유지위해 해당 날짜의 DB만 삭제해보자.
     menu_db.delete()
@@ -37,40 +38,40 @@ def crawl(request):
     employee_house = trs[2]
     tds = employee_house.find_all('td')
     employee_house_menu = tds[date_number + 1].get_text()
-    create_menu_db('교직원_집밥', employee_house_menu)
+    create_menu_db('교직원_집밥', employee_house_menu,today)
 
     employee_oneplate = trs[4]
     tds = employee_oneplate.find_all('td')
     employee_oneplate_menu = tds[date_number + 1].get_text()
-    create_menu_db('교직원_한그릇', employee_oneplate_menu)
+    create_menu_db('교직원_한그릇', employee_oneplate_menu,today)
 
     sangrock_rice = trs[8]
     tds = sangrock_rice.find_all('td')
     sangrock_rice_menu = tds[date_number + 1].get_text()
-    create_menu_db('상록원_백반코너', sangrock_rice_menu)
+    create_menu_db('상록원_백반코너', sangrock_rice_menu,today)
 
     sangrock_oneplate = trs[10]
     tds = sangrock_oneplate.find_all('td')
     sangrock_oneplate_menu = tds[date_number + 1].get_text()
-    create_menu_db('상록원_일품코너', sangrock_oneplate_menu)
+    create_menu_db('상록원_일품코너', sangrock_oneplate_menu,today)
 
     sangrock_cutlet = trs[12]
     tds = sangrock_cutlet.find_all('td')
     sangrock_cutlet_menu = tds[date_number + 1].get_text()
-    create_menu_db('상록원_양식코너', sangrock_cutlet_menu)
+    create_menu_db('상록원_양식코너', sangrock_cutlet_menu,today)
 
     sangrock_head = trs[14]
     tds = sangrock_head.find_all('td')
     sangrock_head_menu = tds[date_number + 1].get_text()
-    create_menu_db('상록원_뚝배기코너', sangrock_head_menu)
+    create_menu_db('상록원_뚝배기코너', sangrock_head_menu,today)
 
     dorm = trs[26]
     tds = dorm.find_all('td')
     dorm_menu = tds[date_number + 1].get_text()
-    create_menu_db('기숙사_A코너', dorm_menu)
+    create_menu_db('기숙사_A코너', dorm_menu,today)
 
 #DB에 저장해주는 녀석
-def create_menu_db(cafe_name, menu):
+def create_menu_db(cafe_name, menu,today):
     Menu.objects.create(
         cafe_name=cafe_name,
         menu=menu,
