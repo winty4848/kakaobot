e264518b3eb4a6cfb9e3ef004a94cee7bd5d5447
winty95 <winty95@nate.com>
2018-08-06 13:36:58 +0900

    crontab이 안되서 crawl 방식 변경

--- kakaobot/urls.py
+++ kakaobot/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 from kakaobot import views
-from . import crawl
+
 urlpatterns = [
     url(r'^keyboard/', views.keyboard),
     url(r'^message', views.answer),
-    url(r'^crawl/', crawl.crawl),
 ]
